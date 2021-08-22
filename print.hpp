6e9c7f02ffec5d8dde63896b7ce15e3b2463c5d7
David C Black <david.black@doulos.com>
2021-08-22 07:20:20 -0500

    Document and cleanup

--- print.hpp
+++ print.hpp
@@ -13,39 +13,43 @@
  *  INFO( "text" )  | Displays a simple informational statement tagged as "Info:"
  *  SHOW( expr )    | Displays expression text and its resultant value
  *  SHOW_PTR( p )   | Displays a pointer address
- *  ECHO( code )    | Raw echo with line number
+ *  ECHO( "text" )  | Raw echo with line number
  *  DO( stmt )      | Echos and executes a statement with line number
  *  __________;     | Displays a horizontal line
+ *  BLANK_LINE;     | Displays a blank line
  */
 namespace {
   const int line_width = 40;
 }
 #ifdef USE_IOSTREAM
-    #include <iostream>
-    #define NOISE(mesg)   std::cout << this << ": " << mesg << '\n'
-    #define INFO(mesg)    std::cout << "Info: " << mesg << '\n'
-    #define SHOW(expr)    std::cout << #expr << " = " << expr << '\n'
-    #define SHOW_PTR(PTR) std::cout << #PTR << " = @" << PTR << '\n'
-    #define ECHO( expr )  std::cout << __LINE__ << ": " << #expr << '\n'
-    #define DO( expr )    std::cout << __LINE__ << ": " << #expr << '\n'; expr
-    #define __________    std::cout << std::string(line_width,'_') << '\n'
+  #include <iostream>
+  #define NOISE(mesg)   std::cout << this << ": " << (mesg) << '\n'
+  #define INFO(mesg)    std::cout << "Info: " << (mesg) << '\n'
+  #define SHOW(expr)    std::cout << #expr << " = " << (expr) << '\n'
+  #define SHOW_PTR(PTR) std::cout << #PTR << " = @" << (PTR) << '\n'
+  #define ECHO( text )  std::cout << __LINE__ << ": " << (text) << '\n'
+  #define DO( stmt )    std::cout << __LINE__ << ": " << #stmt << '\n'; stmt
+  #define __________    std::cout << std::string(line_width,'_') << '\n'
+  #define BLANK_LINE    std::cout << '\n'
 #elif defined(USE_PRINTF)
-    #include <cstdio>
-    #define NOISE(mesg)   std::printf("%p: %s\n", this, mesg)
-    #define INFO(mesg)    std::printf("Info: %s\n", mesg)
-    #define SHOW(expr)    std::printf("%s = %lu\n", #expr, static_cast<uint32_t>(expr))
-    #define SHOW_PTR(PTR) std::printf("%s = @%p\n", #PTR, PTR)
-    #define ECHO( expr )  std::printf("%d: %s\n", __LINE__, #expr )
-    #define DO( expr )    std::printf("%d: %s\n", __LINE__, #expr ); expr
-    #define __________    std::printf("%s\n", std::string(line_width,'_').c_str())
+  #include <cstdio>
+  #define NOISE(mesg)   std::printf("%p: %s\n", this, mesg)
+  #define INFO(mesg)    std::printf("Info: %s\n", mesg)
+  #define SHOW(expr)    std::printf("%s = %u\n", #expr, static_cast<uint32_t>(expr))
+  #define SHOW_PTR(PTR) std::printf("%s = @%p\n", #PTR, PTR)
+  #define ECHO( text )  std::printf("%d: %s\n", __LINE__, text )
+  #define DO( stmt )    std::printf("%d: %s\n", __LINE__, #stmt ); stmt
+  #define __________    std::printf("%s\n", std::string(line_width,'_').c_str())
+  #define BLANK_LINE    std::printf("\n")
 #else
-    #include <string>
-    #include <fmt/format.h>
-    #define NOISE(mesg)   fmt::print("{}: {}\n", fmt::ptr(this), mesg)
-    #define INFO(mesg)    fmt::print("Info: {}\n", mesg)
-    #define SHOW(expr)    fmt::print("{} = {}\n", #expr, expr)
-    #define SHOW_PTR(PTR) fmt::print("{} = @{}\n", #PTR, fmt::ptr(PTR))
-    #define ECHO( expr )  fmt::print("%d: %s\n", __LINE__, #expr ); expr
-    #define DO( expr )    fmt::print("%d: %s\n", __LINE__, #expr ); expr
-    #define __________    fmt::print("{}\n", std::string(line_width,'_'))
+  #include <string>
+  #include <fmt/format.h>
+  #define NOISE(mesg)   fmt::print("{}: {}\n", fmt::ptr(this), mesg)
+  #define INFO(mesg)    fmt::print("Info: {}\n", mesg)
+  #define SHOW(expr)    fmt::print("{} = {}\n", #expr, expr)
+  #define SHOW_PTR(PTR) fmt::print("{} = @{}\n", #PTR, fmt::ptr(PTR))
+  #define ECHO( text )  fmt::print("%d: %s\n", __LINE__, text )
+  #define DO( stmt )    fmt::print("%d: %s\n", __LINE__, #stmt ); stmt
+  #define __________    fmt::print("{}\n", std::string(line_width,'_'))
+  #define BLANK_LINE    fmt::print("\n")
 #endif
